8ce877f8d298d7f30b0bfd8533cbb0fed74671bf
Phill MV <phill.mv@gmail.com>
2009-07-03 03:07:20 -0400

    Now working fine in at least one browser! Man this needs to be in latex.

--- do_it.rb
+++ do_it.rb
@@ -2,13 +2,27 @@ require 'rubygems'
 require 'icalendar'
 require 'haml'
 require 'extensions.rb'
+require 'config.rb'
 require 'sha1'
 
 calendars = Icalendar.parse(File.open('basic.ics'))
 cal = calendars.first
 
 @date_list = Hash.new()
+@template = Haml::Engine.new(File.read("template.haml"){ |io| io.read })
 
+class Day
+  attr_writer :tickets, :date
+  attr_accessor :tickets, :date
+  def initialize
+    @tickets = []
+  end
+end
+
+class Ticket
+  attr_writer :description, :time
+  attr_reader :description, :time
+end
 
 cal.events.each { |e| 
   prop = e.properties
@@ -23,18 +37,6 @@ cal.events.each { |e|
   @date_list[date][task_name] = @date_list[date][task_name] + task_duration
 }
 
-@template = Haml::Engine.new(File.read("template.haml"){ |io| io.read })
-
-@variables = { 
-  :name => "Phillip Mendonça-Vieira",
-  :contact => "(416) 627-3393 • phillmv@okayfail.com • 42 Dubray Ave, M3K1V5 Toronto, ON",
-  :client_name => "The Mark News",
-  :client_contact => "192 Spadina Avenue, Suite 507, M5R2C2 Toronto, ON",
-  
-  # good enough for git, good enough for me ;)
-  :invoice_number => lambda { SHA1.new("#{@variables[:name]}-#{@variables[:client_name]}:#{@variables[:start_date]}-#{@variables[:end_date]}").to_s[0..6] }
-}
-
 def invoice(date, end_date = nil)
   start_date = Date.parse(date)
   end_date ||= start_date + 13
@@ -42,24 +44,43 @@ def invoice(date, end_date = nil)
   @variables[:date] ||= Time.now.to_english
   @variables[:start_date] = start_date.to_english
   @variables[:end_date] = end_date.to_english
-  @variables[:invoice_number] = @variables[:invoice_number].call 
+  @variables[:invoice_number] = @variables[:invoice_number].call
+  @variables[:total_hours] = 0
+  @variables[:days] = []
 
   (start_date..end_date).each { |d|
-    day = d.to_8601
-    if (days_tickets = @date_list[day])
-      puts "=> #{day}"
+   
+    this_day = d.to_8601
+    if (days_tickets = @date_list[this_day])
+      day = Day.new
+      day.date = d
+      @variables[:days] << day
+
       days_tickets.keys.each { |ticket|
-	puts "#{ticket}: #{days_tickets[ticket]/3600.0}"
+	
+	t = Ticket.new
+	t.description = ticket
+
+	t.time = days_tickets[ticket]/3600.0
+	@variables[:total_hours] = @variables[:total_hours] + t.time
+	day.tickets << t
       }
-      puts "\n"
     end
   }
+
+  @variables[:days].each { |d| puts d.date.to_english }
   (end_date + 1).to_8601
   File.open("output/#{start_date.to_8601}_#{end_date.to_8601}.html", "w"){ |io| io.puts @template.render(Object.new, @variables) }
 
 end
 
+def render_days(days)
+  day_template = Haml::Engine.new(File.read("day.haml"){ |io| io.read })
+  str = ""
+  days.each { |day|
+    str = str + day_template.render(Object.new, :day => day)
+  }
+  return str
+end
+
 invoice("2009-04-30")
-
-
-
